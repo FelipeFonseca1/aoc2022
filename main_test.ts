313063364d901dccc7b59761eea6babc720b87a5
FelipeFonseca1 <felipe.fonseca@cronoseuropa.com>
2023-12-04 10:55:54 +0000

    solution runner

--- main_test.ts
+++
@@ -1,7 +0,0 @@
-import { assertEquals } from "https://deno.land/std@0.207.0/assert/mod.ts";
-import { add } from "./main.ts";
-
-Deno.test(function addTest() {
-  assertEquals(add(2, 3), 5);
-  assertEquals(add(1, 1), 2);
-});
