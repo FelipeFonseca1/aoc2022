bee84d83c15d89bd91126147338786324fa949df
FelipeFonseca1 <felipe.fonseca@cronoseuropa.com>
2023-12-05 08:27:54 +0000

    clean up

--- utils/mod.ts
+++ utils/mod.ts
@@ -1,4 +1,4 @@
-export {enumerate, range, first } from "https://deno.land/x/itertools/mod.ts";
+export {enumerate, range, first } from "https://deno.land/x/itertools@v1.1.1/mod.ts";
 export { split_lines, split_words, split_letters, split_blocks } from "./input.ts";
 
 export function read_input(file_name: string): Promise<string> {
